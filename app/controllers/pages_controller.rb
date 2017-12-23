19c967aa31a5a6fa898effd4cc18a96551c901b8
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-12-23 17:59:46 +0100

    pry bybug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,6 @@ class PagesController < ApplicationController
       config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
       config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
     end
-    @client = client.search("#ripple", lang: "fr").count
+    @clients = client.search("#ripple", lang: "fr")
   end
 end
