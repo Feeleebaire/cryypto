a36407367c98d6fd9d5b08efb955dcce3cc9adac
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-12-25 11:21:31 +0100

    redierct path work

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,9 +1,16 @@
 class CryptosController < ApplicationController
 
+  def index
+    @cryptos = Crypto.all
+  end
+
+  def show
+  end
 
   private
 
   def crypto_params
     params.require(:crypto).permit(:name, :photo, :photo_cache)
   end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,7 @@ require 'open-uri'
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
-
   def home
-
     @cryptos = Crypto.all
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
@@ -13,13 +11,11 @@ class PagesController < ApplicationController
       config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
       config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
     end
-
     @clients = client.search("#ripple")
     @bitcoin = client.search("#ETH")
 
     url = 'https://api.github.com/orgs/ripple'
     user_serialized = open(url).read
     @user = JSON.parse(user_serialized)
-
   end
 end
